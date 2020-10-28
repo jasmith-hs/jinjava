fd4b29e4ed15dd0e2f5982484af9cc38931b3057
Jack Smith <jasmith@hubspot.com>
2020-10-28 14:30:08 -0400

    Add simple eager unless tag test

--- src/test/java/com/hubspot/jinjava/lib/tag/eager/EagerUnlessTagTest.java
+++ src/test/java/com/hubspot/jinjava/lib/tag/eager/EagerUnlessTagTest.java
@@ -1,12 +1,17 @@
 package com.hubspot.jinjava.lib.tag.eager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.hubspot.jinjava.ExpectedNodeInterpreter;
 import com.hubspot.jinjava.JinjavaConfig;
 import com.hubspot.jinjava.interpret.DeferredValue;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.lib.tag.UnlessTagTest;
+import com.hubspot.jinjava.tree.parse.TagToken;
+import java.util.Optional;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EagerUnlessTagTest extends UnlessTagTest {
   private ExpectedNodeInterpreter expectedNodeInterpreter;
@@ -30,5 +35,32 @@ public class EagerUnlessTagTest extends UnlessTagTest {
   @After
   public void teardown() {
     JinjavaInterpreter.popCurrent();
+  }
+
+  @Test
+  public void itHandlesDeferredInRegular() {
+    context.put("foo", true);
+    expectedNodeInterpreter.assertExpectedOutput("handles-deferred-in-regular");
+    Optional<EagerToken> maybeEagerTagToken = context
+      .getEagerTokens()
+      .stream()
+      .filter(e -> ((TagToken) e.getToken()).getTagName().equals(tag.getName()))
+      .findAny();
+    assertThat(maybeEagerTagToken).isEmpty();
+  }
+
+  @Test
+  public void itHandlesDeferredInEager() {
+    context.put("foo", 1);
+    expectedNodeInterpreter.assertExpectedOutput("handles-deferred-in-eager");
+    Optional<EagerToken> maybeEagerTagToken = context
+      .getEagerTokens()
+      .stream()
+      .filter(e -> ((TagToken) e.getToken()).getTagName().equals(tag.getName()))
+      .findAny();
+    assertThat(maybeEagerTagToken).isPresent();
+    assertThat(maybeEagerTagToken.get().getSetDeferredWords()).isEmpty();
+    assertThat(maybeEagerTagToken.get().getUsedDeferredWords())
+      .containsExactlyInAnyOrder("deferred");
   }
 }
